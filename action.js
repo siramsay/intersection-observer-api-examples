81cc6fcb7673271b60094ca25280d08fefa0f661
siramsay <simon@designkojo.net>
2025-05-01 12:54:21 +0900

    Add foreach on query selector all

--- action.js
+++ action.js
@@ -1,4 +1,7 @@
-const three = document.getElementById('three');
+//const three = document.getElementById('three');
+//const three = document.querySelector('.three');
+//const three = document.querySelectorAll('.three');
+console.log(three);
 
 let observer;
 
@@ -9,7 +12,14 @@ let options = {
 };
 
 observer = new IntersectionObserver( handleIntersect , options );
-observer.observe(three);
+//observer.observe(three.item(0));
+
+// https://stackoverflow.com/questions/54866560/same-intersection-observer-for-multiple-html-elements
+document.querySelectorAll('.three').forEach((i) => {
+  if (i) {
+    observer.observe(i);
+  }
+});
 
 function handleIntersect(changes, observer) {
 
